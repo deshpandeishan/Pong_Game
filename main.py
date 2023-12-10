5a1d884e3ce8fc2a76966c9e7fc1ff7fa91ae0dc
Ishan Deshpande <deshpandeishan75@gmail.com>
2023-12-10 19:33:57 +0530

    Added movement to the paddle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Screen
+import turtle
+from turtle import Turtle, Screen
 from scoreboards import LeftScoreBoard, RightScoreBoard, MiddleLine
-from paddles import Paddle
+# from paddles import Paddle
 # import time
 
 screen = Screen()
@@ -9,14 +10,39 @@ screen.title("Pong Game")
 screen.bgcolor("black")
 
 
-sb_1 = LeftScoreBoard()
-sb_2 = RightScoreBoard()
-middle_line = MiddleLine()
-paddle = Paddle()
+# sb_1 = LeftScoreBoard()
+# sb_2 = RightScoreBoard()
+# middle_line = MiddleLine()
 
-sb_1.increase_scores()
-sb_2.increase_scores()
-middle_line.draw_line()
-# paddle.setup()
+screen.tracer(False)
+
+
+def go_up():
+    y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), y)
+
+
+def go_down():
+    y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), y)
+
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(5, 1)
+paddle.color("white")
+paddle.penup()
+paddle.goto(350, 0)
+screen.tracer(True)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+# sb_1.increase_scores()
+# sb_2.increase_scores()
+# middle_line.draw_line()
+
 
 screen.exitonclick()
--- paddles.py
+++ paddles.py
@@ -1,20 +1,15 @@
 from turtle import Turtle
 
+paddle = Turtle()
+
 
 class Paddle(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.goto(10, 0)
-        self.lt(90)
-
-    def setup(self):
-        self.color("white")
         self.shape("square")
-        self.shapesize(1, 3)
+        self.shapesize(5, 1)
+        self.color("white")
+        self.penup()
+        self.goto(350, 0)
 
-    # def move_left(self):
-    #     self.goto(self.x_cor, self.y_cor + 30)
-    #
-    # def move_right(self):
-    #     self.goto(self.x_cor, self.y_cor + -30)
