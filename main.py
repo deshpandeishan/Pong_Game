6f8c63dcdfa8c7807822217888e181dab4276a61
Ishan Deshpande <deshpandeishan75@gmail.com>
2023-12-11 22:08:56 +0530

    Added classes.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Screen
-from scoreboards import LeftScoreBoard, RightScoreBoard, MiddleLine
+from scoreboards import ScoreBoard, MiddleLine
 from paddles import Paddle
 
 screen = Screen()
@@ -8,8 +8,8 @@ screen.title("Pong Game")
 screen.bgcolor("black")
 
 screen.tracer(False)
-sb_1 = LeftScoreBoard()
-sb_2 = RightScoreBoard()
+sb_1 = ScoreBoard(-100, 300)
+sb_2 = ScoreBoard(100, 300)
 middle_line = MiddleLine()
 
 paddle1 = Paddle()
@@ -22,8 +22,6 @@ paddle2.setup(400, 0)
 paddle2.track_right()
 screen.tracer(True)
 
-sb_1.increase_scores()
-sb_2.increase_scores()
 middle_line.draw_middle_line()
 
 screen.exitonclick()
--- scoreboards.py
+++ scoreboards.py
@@ -22,43 +22,20 @@ class MiddleLine(Turtle):
             self.pendown()
 
 
-class LeftScoreBoard(Turtle):
+class ScoreBoard(Turtle):
 
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
         self.screen.tracer(False)
-        self.goto(-100, 300)
+        self.goto(x, y)
         self.color("white")
         self.penup()
         self.score_1 = 0
-        self.update_left_scores()
-        self.hideturtle()
-
-    def increase_scores(self):
-        self.score_1 += 1
-        self.clear()
-        self.update_left_scores()
-
-    def update_left_scores(self):
-        self.write(f"{self.score_1}", align='center', font=('Courier', 30, 'normal'))
-
-
-class RightScoreBoard(Turtle):
-
-    def __init__(self):
-        super().__init__()
-        self.screen.tracer(False)
-        self.goto(100, 300)
-        self.color("white")
-        self.penup()
         self.score_2 = 0
-        self.update_right_scores()
+        self.scores = 0
+        self.update_left_scores(self.scores)
         self.hideturtle()
 
-    def increase_scores(self):
-        self.score_2 += 1
-        self.clear()
-        self.update_right_scores()
+    def update_left_scores(self, scores):
+        self.write(f"{self.scores}", align='center', font=('Courier', 30, 'normal'))
 
-    def update_right_scores(self):
-        self.write(f"{self.score_2}", align='center', font=('Courier', 30, 'normal'))
